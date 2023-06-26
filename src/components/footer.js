e1412b21a10747f59553aa934e4e116d6d50941a
Paronim <Zreba.v@gmial.com>
2023-06-27 00:33:52 +0300

    Стили footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,11 +16,11 @@ export function createFooter() {
 
     logo.src = '/public/assets/icons/logo-footer.svg'
 
-    list.innerHTML = '<li class="element-list-footer"><a href="#">О клинике</a></li>' +
-                    '<li class="element-list-footer"><a href="#">Услуги</a></li>' +
-                    '<li class="element-list-footer"><a href="#">Специалисты</a></li>' + 
-                    '<li class="element-list-footer"><a href="#">Цены</a></li>' +
-                    '<li class="element-list-footer"><a href="#">Контакты</a></li>'
+    list.innerHTML = '<li class="element-list-footer"><a class="element-list-footer-a" href="#">О клинике</a></li>' +
+                    '<li class="element-list-footer"><a class="element-list-footer-a" href="#">Услуги</a></li>' +
+                    '<li class="element-list-footer"><a class="element-list-footer-a" href="#">Специалисты</a></li>' + 
+                    '<li class="element-list-footer"><a class="element-list-footer-a" href="#">Цены</a></li>' +
+                    '<li class="element-list-footer"><a class="element-list-footer-a" href="#">Контакты</a></li>'
 
     socialNet.innerHTML = '<a class="image-footer-first" href="#"><img src="/public/assets/icons/instagram.svg"/></a>' +
                         '<a class="image-footer" href="#"><img src="/public/assets/icons/watsapp.svg"/></a>' +
