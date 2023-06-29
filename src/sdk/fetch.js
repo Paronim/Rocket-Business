820585e878544dfb09c713d99ba029f7efcd9c5d
Paronim <Zreba.v@gmial.com>
2023-06-29 14:31:09 +0300

    Проект закончен!!!

--- src/sdk/fetch.js
+++ src/sdk/fetch.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 import {storeSet} from '../store/store.js'
 
 export async function requestFeed () {
-    await axios.get('https://github.com/Paronim/Rocket-Business/tree/main/src/data/feed.jcon').then(res => {
+    await axios.get('https://raw.githubusercontent.com/Paronim/Rocket-Business/main/src/data/feed.json').then(res => {
         storeSet(res)
     }).catch(error => console.log(error))
 }
 
 export async function requestSlider () {
-    await axios.get('https://github.com/Paronim/Rocket-Business/tree/main/src/data/slider.jcon').then(res => {
+    await axios.get('https://raw.githubusercontent.com/Paronim/Rocket-Business/main/src/data/slider.json').then(res => {
         storeSet(res)
     }).catch(error => console.log(error))
 }
